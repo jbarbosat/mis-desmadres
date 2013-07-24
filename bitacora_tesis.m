0ac30e5b624452b8a0c5f9014fed09f126f9349b
Jessica Barbosa <pandorabob@gmail.com>
2013-07-24 00:47:41 -0500

    Create bitacora_tesis.m

---
+++ bitacora_tesis.m
@@ -0,0 +1,12 @@
+Julio 22, 2013
+
+Nos vimos Adolfo, David y yo para ver q onda.
+Dropbox con una muestra de los datos que básicamente corresponden a dos días.
+
+David se va a ir de viaje así q estos días hay que espiar los datos y tener ideas inteligentes.
+
+Próxima semana nos veremos.
+
+Son datos de ventas de medicinas entre los laboratorios y las farmacias. Hay intermediarios que hacen eso.
+Hay promociones y cosas así. Hay que decir cosas interesantes para que se conserven esos datos. 
+
