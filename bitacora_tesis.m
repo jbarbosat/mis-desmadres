f58873cac51c5d0ec618a95569912bfa80a791f6
Jessica Barbosa <pandorabob@gmail.com>
2013-08-01 02:42:42 -0500

    reunión 2

--- bitacora_tesis.m
+++ bitacora_tesis.m
@@ -1,4 +1,5 @@
 Julio 22, 2013
+---------------
 
 Nos vimos Adolfo, David y yo para ver q onda.
 Dropbox con una muestra de los datos que básicamente corresponden a dos días.
@@ -10,3 +11,34 @@ Próxima semana nos veremos.
 Son datos de ventas de medicinas entre los laboratorios y las farmacias. Hay intermediarios que hacen eso.
 Hay promociones y cosas así. Hay que decir cosas interesantes para que se conserven esos datos. 
 
+
+Julio 31, 2013
+--------------
+
+Nos vimos David y yo para rebotar ideas y ver qué onda. 
+Chance en dos días veamos al líder del proyecto que tiene ideas interesantes.
+
+Necesitamos enfocarnos en que se ahorren baro si hacen tal o cual cosa. O que ganen más baro si hacen bla bla. 
+
+
+Ideas:
+
+- Se nos ocurrió que de tener la matriz de productos-facturas podríamos factorizarla como vimos con Felipe y chance sirva.
+
+- Tbn pensamos que se podría hacer market basket analysis.
+
+
+Tarea:
+
+-Necesito hacer una gráfica de clientes y tipos de transacciones y que el grosor de las aristas esté dado por
+la suma de los importes que caen en ese tipo de operación: venta, descuento o promoción (importe >0, <0 ó =0)
+(aunq en la base todo es positivo... así que podría hacerse no por tipo sino tal cual por importe)
+
+- Tbn ver qué puedo hacer por grupo de clientes. C, M, P, etc.
+
+
+Problemas y preguntas:
+
+- David dijo que venta, descuento o promoción (importe >0, <0 ó =0) pero en la base todo es positivo. 
+- Tenemos una muestra cero representativa...
+- Haace falta entender mejor las variables para ver qué es importante.
