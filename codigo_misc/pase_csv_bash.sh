b13cb1f762e712ba806d44b1f27d7b9be55aa0c7
Jessica Barbosa <pandorabob@gmail.com>
2022-10-14 19:04:49 +0100

    Update pase_csv_bash.sh

--- codigo_misc/pase_csv_bash.sh
+++ codigo_misc/pase_csv_bash.sh
@@ -1,3 +1,11 @@
-while IFS=, read -r col1 col2; do
-  echo $col1
-done < file.csv
+rm matches.txt
+while IFS=' ' read -r date time size path
+do
+    # echo "$date $time $size $path"
+    echo "$path"
+    num_matches=$(aws s3 cp s3://come_bucket/$path - --profile heritage_dagger| grep 'some_string' | wc -l)
+    echo "$num_matches"
+    if [[ $num_matches -ne "0" ]]; then
+    echo $path >> matches.txt
+    fi
+done < exact_log_names.txt
